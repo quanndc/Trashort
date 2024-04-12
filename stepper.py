23735f59f7c99729e018bd9260fd63d25dc03dca
Gideon <tranminhquan.dtp1b@gmail.com>
2024-04-12 20:20:11 +0700

    push

--- stepper.py
+++ stepper.py
@@ -1,33 +1,32 @@
-from rpi_python_drv8825.stepper import StepperMotor
 from time import sleep
-# GPIO setup
-enable_pin = 4
-step_pin = 18
-dir_pin = 24
-mode_pins = (21, 22, 27)
-# Stepper motor setup
-step_type = '1/32'
-fullstep_delay = .005
+import RPi.GPIO as GPIO
 
+DIR = 24   # Direction GPIO Pin
+STEP = 18  # Step GPIO Pin
+CW = 1     # Clockwise Rotation
+CCW = 0    # Counterclockwise Rotation
+SPR = 48   # Steps per Revolution (360 / 7.5)
 
-# create object
-motor = StepperMotor(enable_pin, step_pin, dir_pin, mode_pins, step_type, fullstep_delay)
-motor.enable(True)
-for i in range(100):
-    
-    motor.enable(True)        # enables stepper driver
-    motor.run(200*32, True)     # run motor 6400 steps clowckwise
-    total_microsteps = 200*32 * 1/32
-    # print(total_microsteps)
-    print(i+1.8)
-    actual_rotation_angle = float(total_microsteps) * 1.8
-    print("quay" + str(actual_rotation_angle))
-    #sleep(0.5)
-    motor.run(200*32, False)  
-    total_microsteps = 200*32 * 1/32
-    # print(total_microsteps)
-    print(i+1.8)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(DIR, GPIO.OUT)
+GPIO.setup(STEP, GPIO.OUT)
+GPIO.output(DIR, CW)
 
-    actual_rotation_angle = float(total_microsteps) * 1.8
-    print("quay" + str(actual_rotation_angle))  # run motor 6400 steps counterclockwise
-    #motor.enable(False)       # disable stepper driver
+step_count = SPR
+delay = .0208
+
+for x in range(step_count):
+    GPIO.output(STEP, GPIO.HIGH)
+    sleep(delay)
+    GPIO.output(STEP, GPIO.LOW)
+    sleep(delay)
+
+sleep(.5)
+GPIO.output(DIR, CCW)
+for x in range(step_count):
+    GPIO.output(STEP, GPIO.HIGH)
+    sleep(delay)
+    GPIO.output(STEP, GPIO.LOW)
+    sleep(delay)
+
+GPIO.cleanup()
