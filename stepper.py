d4d75db89bac82e6aeefecf8ef8a0992ec82e069
Gideon <tranminhquan.dtp1b@gmail.com>
2024-04-12 19:37:31 +0700

    push

--- stepper.py
+++ stepper.py
@@ -1,46 +1,86 @@
+# This Raspberry Pi code was developed by newbiely.com
+# This Raspberry Pi code is made available for public use without any restriction
+# For comprehensive instructions and wiring diagrams, please visit:
+# https://newbiely.com/tutorials/raspberry-pi/raspberry-pi-28byj-48-stepper-motor-uln2003-driver
+
+
 import RPi.GPIO as GPIO
+import time
 
-# import the library
-from RpiMotorLib import RpiMotorLib
-    
-#define GPIO pins
-GPIO_pins = (21, 22, 27) # Microstep Resolution MS1-MS3 -> GPIO Pin
-direction= 20       # Direction -> GPIO Pin
-step = 21      # Step -> GPIO Pin
+# Define GPIO pins for ULN2003 driver
+IN1 = 23
+IN2 = 24
+IN3 = 25
+IN4 = 8
 
-# Declare a instance of class pass GPIO pins numbers and the motor type
-mymotortest = RpiMotorLib.A4988Nema(direction, step, GPIO_pins, "DRV8825")
+# Set GPIO mode and configure pins
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(IN1, GPIO.OUT)
+GPIO.setup(IN2, GPIO.OUT)
+GPIO.setup(IN3, GPIO.OUT)
+GPIO.setup(IN4, GPIO.OUT)
 
+# Define constants
+DEG_PER_STEP = 1.8
+STEPS_PER_REVOLUTION = int(360 / DEG_PER_STEP)
 
-# call the function, pass the arguments
-mymotortest.motor_go(False, "Full" , 100, .01, False, .05)
-from rpi_python_drv8825.stepper import StepperMotor
-from time import sleep
-# GPIO setup
-enable_pin = 4
-step_pin = 18
-dir_pin = 24
-mode_pins = (21, 22, 27)
-# Stepper motor setup
-step_type = '1/64'
-fullstep_delay = .005
+# Define sequence for 28BYJ-48 stepper motor
+seq = [
+    [1, 0, 0, 1],
+    [1, 0, 0, 0],
+    [1, 1, 0, 0],
+    [0, 1, 0, 0],
+    [0, 1, 1, 0],
+    [0, 0, 1, 0],
+    [0, 0, 1, 1],
+    [0, 0, 0, 1]
+]
 
+# Function to rotate the stepper motor one step
+def step(delay, step_sequence):
+    for i in range(4):
+        GPIO.output(IN1, step_sequence[i][0])
+        GPIO.output(IN2, step_sequence[i][1])
+        GPIO.output(IN3, step_sequence[i][2])
+        GPIO.output(IN4, step_sequence[i][3])
+        time.sleep(delay)
 
-# create object
-motor = StepperMotor(enable_pin, step_pin, dir_pin, mode_pins, step_type, fullstep_delay)
-motor.enable(True)
-for i in range(100):
-    
-    motor.enable(True)        # enables stepper driver
-    motor.run(200*32, True)     # run motor 6400 steps clowckwise
-    total_microsteps = 200*32 * 1/32
-    print(total_microsteps)
-    actual_rotation_angle = float(total_microsteps) * 1.8
-    print("quay" + str(actual_rotation_angle))
-    #sleep(0.5)
-    motor.run(200*32, False)  
-    total_microsteps = 200*32 * 1/32
-    print(total_microsteps)
-    actual_rotation_angle = float(total_microsteps) * 1.8
-    print("quay" + str(actual_rotation_angle))  # run motor 6400 steps counterclockwise
-    #motor.enable(False)       # disable stepper driver
+# Function to move the stepper motor one step forward
+def step_forward(delay, steps):
+    for _ in range(steps):
+        step(delay, seq[0])
+        step(delay, seq[1])
+        step(delay, seq[2])
+        step(delay, seq[3])
+
+# Function to move the stepper motor one step backward
+def step_backward(delay, steps):
+    for _ in range(steps):
+        step(delay, seq[3])
+        step(delay, seq[2])
+        step(delay, seq[1])
+        step(delay, seq[0])
+
+try:
+    # Set the delay between steps
+    delay = 0.005
+
+    while True:
+        # Rotate one revolution forward (clockwise)
+        step_forward(delay, STEPS_PER_REVOLUTION)
+
+        # Pause for 2 seconds
+        time.sleep(2)
+
+        # Rotate one revolution backward (anticlockwise)
+        step_backward(delay, STEPS_PER_REVOLUTION)
+
+        # Pause for 2 seconds
+        time.sleep(2)
+
+except KeyboardInterrupt:
+    print("\nExiting the script.")
+
+finally:
+    # Clean up GPIO settings
+    GPIO.cleanup()
