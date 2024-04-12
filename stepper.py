39280bd2ea181716d0afcecee7fe069aac653bb6
Gideon <tranminhquan.dtp1b@gmail.com>
2024-04-12 18:16:31 +0700

    push

--- stepper.py
+++ stepper.py
@@ -12,13 +12,13 @@ fullstep_delay = 2
 
 # create object
 motor = StepperMotor(enable_pin, step_pin, dir_pin, mode_pins, step_type, fullstep_delay)
-motor.enable(True)
-for i in range(100):
-        motor.enable(True)        # enables stepper driver
-        motor.run(200*32, True)     # run motor 6400 steps clowckwise
-        total_microsteps = 200*32 * 1/32
-        actual_rotation_angle = float(total_microsteps) * 1.8
-        print("quay" + str(actual_rotation_angle))
-        #sleep(0.5)
-        motor.run(200*32, False)    # run motor 6400 steps counterclockwise
-        #motor.enable(False)       # disable stepper driver
+# motor.enable(True)
+# for i in range(100):
+motor.enable(True)        # enables stepper driver
+motor.run(200*32, True)     # run motor 6400 steps clowckwise
+total_microsteps = 200*32 * 1/32
+actual_rotation_angle = float(total_microsteps) * 1.8
+print("quay" + str(actual_rotation_angle))
+#sleep(0.5)
+motor.run(200*32, False)    # run motor 6400 steps counterclockwise
+#motor.enable(False)       # disable stepper driver
