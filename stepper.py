23474d41d2cc1656da33f1706c8c3a120dbfcdb9
Gideon <tranminhquan.dtp1b@gmail.com>
2024-05-07 18:23:20 +0700

    add driver + detect

--- stepper.py
+++ stepper.py
@@ -36,8 +36,36 @@
 
 # import the opencv library 
 import cv2 
-  
 from lobe import ImageModel
+import time
+from rpi_python_drv8825.stepper import StepperMotor
+from time import sleep
+# GPIO setup
+enable_pin = 4
+step_pin = 18
+dir_pin = 24
+mode_pins = (21, 22, 27)
+# Stepper motor setup
+step_type = '1/32'
+fullstep_delay = .005
+
+# create object
+motor = StepperMotor(enable_pin, step_pin, dir_pin, mode_pins, step_type, fullstep_delay)
+# motor.enable(True)
+# for i in range(100):
+
+    # motor.enable(True)     # enables stepper driver
+    # motor.run(200*32, True)     # run motor 6400 steps clowckwise
+    # total_microsteps = 200*32 * 1/32
+    # print(total_microsteps)
+    # print(i+1.8)
+    # actual_rotation_angle = float(total_microsteps) * 1.8
+    # print("quay" + str(actual_rotation_angle))
+    #sleep(0.5)
+    # motor.run(200*32, False)  
+    # total_microsteps = 200*32 * 1/32
+    # print(total_microsteps)
+    # print(i+1.8)
 # define a video capture object 
 vid = cv2.VideoCapture(0) 
 
@@ -60,12 +88,16 @@ while(True):
         cv2.imwrite('../Pictures/image.jpg', frame)
         result = model.predict_from_file('../Pictures/image.jpg')
         print(result.prediction)
+        motor.enable(True)        # enables stepper driver
+        motor.run(200*32, True)
+        motor.run(200*32, False)  
+        motor.enable(False)        # disables stepper driver
         
       
     # the 'q' button is set as the 
     # quitting button you may use any 
     # desired button of your choice 
-    if cv2.waitKey(1) & 0xFF == ord('q'): 
+    if cv2.waitKey(2) & 0xFF == ord('q'): 
         break
   
 
