a8bfe98130fb261f0b6c0f2e3ceb75a237e0a53c
Gideon <tranminhquan.dtp1b@gmail.com>
2024-05-07 14:05:37 +0700

    first commit

--- stepper.py
+++ stepper.py
@@ -1,32 +1,42 @@
-from time import sleep
-import RPi.GPIO as GPIO
+# from time import sleep
+# import RPi.GPIO as GPIO
 
-DIR = 24   # Direction GPIO Pin
-STEP = 18  # Step GPIO Pin
-CW = 1     # Clockwise Rotation
-CCW = 0    # Counterclockwise Rotation
-SPR = 48   # Steps per Revolution (360 / 7.5)
+# DIR = 24   # Direction GPIO Pin
+# STEP = 18  # Step GPIO Pin
+# CW = 1     # Clockwise Rotation
+# CCW = 0    # Counterclockwise Rotation
+# SPR = 48   # Steps per Revolution (360 / 7.5)
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(DIR, GPIO.OUT)
-GPIO.setup(STEP, GPIO.OUT)
-GPIO.output(DIR, CW)
+# GPIO.setmode(GPIO.BCM)
+# GPIO.setup(DIR, GPIO.OUT)
+# GPIO.setup(STEP, GPIO.OUT)
+# GPIO.output(DIR, CW)
 
-step_count = SPR
-delay = .0208
+# step_count = SPR
+# delay = .0208
 
-for x in range(step_count):
-    GPIO.output(STEP, GPIO.HIGH)
-    sleep(delay)
-    GPIO.output(STEP, GPIO.LOW)
-    sleep(delay)
+# for x in range(step_count):
+#     GPIO.output(STEP, GPIO.HIGH)
+#     sleep(delay)
+#     GPIO.output(STEP, GPIO.LOW)
+#     sleep(delay)
 
-sleep(.5)
-GPIO.output(DIR, CCW)
-for x in range(step_count):
-    GPIO.output(STEP, GPIO.HIGH)
-    sleep(delay)
-    GPIO.output(STEP, GPIO.LOW)
-    sleep(delay)
+# sleep(.5)
+# GPIO.output(DIR, CCW)
+# for x in range(step_count):
+#     GPIO.output(STEP, GPIO.HIGH)
+#     sleep(delay)
+#     GPIO.output(STEP, GPIO.LOW)
+#     sleep(delay)
 
-GPIO.cleanup()
+# GPIO.cleanup()
+
+
+#open camera with opencv
+
+import cv2
+import numpy as np
+import time
+
+cap = cv2.VideoCapture(0)
+time.sleep(10)
