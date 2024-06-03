27c963ea8a0f25ff3232aa87dd5277e2f2f2ebf9
Gideon <tranminhquan.dtp1b@gmail.com>
2024-06-03 14:30:53 +0700

    change loop capture

--- dataset_script.py
+++ dataset_script.py
@@ -12,26 +12,26 @@ def get_miliseconds():
     return int(time.time() * 1000)
 
 
-while True:
-  ret, frame = vid.read()
-        # disable mirror effect
-  frame = cv2.flip(frame, 1)
-        # show the frame
-#   cv2.imshow("frame", frame)
-  for interval in IntervalTimer(10):
-        # Capture the video frame by frame
+for interval in IntervalTimer(10):
+    ret, frame = vid.read()
+    # disable mirror effect
+    frame = cv2.flip(frame, 1)
+    # show the frame
+    #   cv2.imshow("frame", frame)
+
+    # Capture the video frame by frame
     print("Capturing image...")
 
     # capture image
     # get the current time
     current_time = get_miliseconds()
     # cv2.imwrite(str(current_time) + ".jpg", frame)
-    cv2.imwrite("/home/trashort/Pictures/" + str(current_time) + '.jpg', frame)
-    with open("/home/trashort/Pictures/"+str(current_time)+ '.jpg', "rb") as f:
+    cv2.imwrite("/home/trashort/Pictures/" + str(current_time) + ".jpg", frame)
+    with open("/home/trashort/Pictures/" + str(current_time) + ".jpg", "rb") as f:
         img_data = f.read()
-        img_name = str(current_time) + '.jpg'
+        img_name = str(current_time) + ".jpg"
 
     r = requests.post(url + "uploadRecycle", files={"file": img_data})
-    os.remove("/home/trashort/Pictures/" + str(current_time) + '.jpg')
+    os.remove("/home/trashort/Pictures/" + str(current_time) + ".jpg")
     if r.status_code == 201:
         print("Image uploaded successfully!")
