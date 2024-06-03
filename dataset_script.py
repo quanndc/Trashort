6ef1521cfce77a8c663ef8b2e105fcdf2fd84bc5
Gideon <tranminhquan.dtp1b@gmail.com>
2024-06-03 12:40:27 +0700

    migrate to raspberrypi

--- dataset_script.py
+++ dataset_script.py
@@ -1,28 +1,43 @@
-import pyrebase
+import requests
+import cv2
 import time
+import os
+
+url = "http://localhost:1323/"
+vid = cv2.VideoCapture(1)
 
 
-firebaseConfig = {
-  apiKey: "AIzaSyBg8NoVuSXHBzoQ0i1W_V62oBI2B_PJHWU",
-  authDomain: "trashort-26c32.firebaseapp.com",
-  projectId: "trashort-26c32",
-  storageBucket: "trashort-26c32.appspot.com",
-  messagingSenderId: "261465509828",
-  appId: "1:261465509828:web:6f964ac329833f06f2de91"
-};
-
-firebase = pyrebase.initialize_app(firebaseConfig)
-bucket =  firebase.storage()
+def get_miliseconds():
+    return int(time.time() * 1000)
 
 
+while True:
+  ret, frame = vid.read()
+        # disable mirror effect
+  frame = cv2.flip(frame, 1)
+        # show the frame
+  cv2.imshow("frame", frame)
+  if cv2.waitKey(1) & 0xFF == ord("c"):
+        # Capture the video frame by frame
+        
 
+    # capture image
+    # get the current time
+    current_time = get_miliseconds()
+    # cv2.imwrite(str(current_time) + ".jpg", frame)
+    cv2.imwrite("/home/trashort/Pictures/" + str(current_time) + '.jpg', frame)
+    with open("/home/trashort/Pictures/"+str(current_time)+ '.jpg', "rb") as f:
+        img_data = f.read()
+        img_name = str(current_time) + '.jpg'
 
-# post an image to bucket
-def post_image(image_path):
-    # post the image to the bucket
-    bucket.child("images").put(image_path)
-    # get the url of the image
-    url = bucket.child("images").child(image_path).get_url(None)
-    return url
-
-post_image('/home/trashort/Pictures/default_background/default_background.jpg')
+    r = requests.post(url + "uploadRecycle", files={"file": img_data})
+    os.remove("/home/trashort/Pictures/" + str(current_time) + '.jpg')
+    if r.status_code == 201:
+        print("Image uploaded successfully!")
+        
+  if cv2.waitKey(1) & 0xFF == ord("q"):
+    break
+# After the loop release the cap object
+vid.release()
+# Destroy all the windows
+cv2.destroyAllWindows()
