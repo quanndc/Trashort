b354f436d4bd22a58668555da6a1714a99a1fe22
Gideon <tranminhquan.dtp1b@gmail.com>
2024-06-13 15:30:26 +0700

    test background and frame shape

--- stepper.py
+++ stepper.py
@@ -123,8 +123,8 @@ for interval in IntervalTimer(10):
     frame = cv2.flip(frame, 1)
     # frameForCheck = frame
     #capture image
-    cv2.imwrite("/home/trashort/Pictures/image.jpg", frame)
-    pic = cv2.imread("/home/trashort/Pictures/image.jpg")
+    # cv2.imwrite("/home/trashort/Pictures/image.jpg", frame)
+    # pic = cv2.imread("/home/trashort/Pictures/image.jpg")
     #get background image
     background = cv2.imread("/home/trashort/Pictures/default_background/image.jpg")
     # resize the background to 400x400
@@ -134,7 +134,7 @@ for interval in IntervalTimer(10):
     # diffPoints = checkBackground(background, frame)
     print(diffPoints)
     print(background.shape)
-    print(pic.shape)
+    print(frame.shape)
     if diffPoints < 1:
         print("Background is the same as default background")
         os.remove("/home/trashort/Pictures/image.jpg")
