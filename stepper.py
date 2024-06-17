24fefc2a6879386fa1aa6c1350c5e249fe141891
Gideon <tranminhquan.dtp1b@gmail.com>
2024-06-17 16:29:19 +0700

    stop service and test file

--- stepper.py
+++ stepper.py
@@ -1,91 +1,91 @@
-# import libs
-from rpi_python_drv8825.stepper import StepperMotor # type: ignore
-from interval_timer import IntervalTimer
-from tflite_runtime.interpreter import Interpreter # type: ignore
-import numpy as np
-import cv2 as cv
-import os
+# # import libs
+# from rpi_python_drv8825.stepper import StepperMotor # type: ignore
+# from interval_timer import IntervalTimer
+# from tflite_runtime.interpreter import Interpreter # type: ignore
+# import numpy as np
+# import cv2 as cv
+# import os
 
-# GPIO setup
-enable_pin = 4
-step_pin = 18
-dir_pin = 24
-mode_pins = (21, 22, 27)
-# Stepper motor setup
-step_type = "1/32"
-fullstep_delay = 0.005
-# create motor object
-motor = StepperMotor(enable_pin, step_pin, dir_pin, mode_pins, step_type, fullstep_delay)
+# # GPIO setup
+# enable_pin = 4
+# step_pin = 18
+# dir_pin = 24
+# mode_pins = (21, 22, 27)
+# # Stepper motor setup
+# step_type = "1/32"
+# fullstep_delay = 0.005
+# # create motor object
+# motor = StepperMotor(enable_pin, step_pin, dir_pin, mode_pins, step_type, fullstep_delay)
 
-# define a video capture object
-# vid = cv2.VideoCapture(0)
+# # define a video capture object
+# # vid = cv2.VideoCapture(0)
 
-# project folder
-project_folder = "/home/trashort/Repos/Trashort/"
-# model path
-model_path = project_folder + "model.tflite"
-label_path = project_folder + "labels.txt"
-
-
-def load_labels(path):  # Read the labels from the text file as a Python list.
-    with open(path, "r") as f:
-        return [line.strip() for i, line in enumerate(f.readlines())]
-
-def set_input_tensor(interpreter, image):
-    tensor_index = interpreter.get_input_details()[0]["index"]
-    input_tensor = interpreter.tensor(tensor_index)()[0]
-    input_tensor[:, :] = image
+# # project folder
+# project_folder = "/home/trashort/Repos/Trashort/"
+# # model path
+# model_path = project_folder + "model.tflite"
+# label_path = project_folder + "labels.txt"
 
 
-def classify_image(interpreter, image, top_k=1):
-    set_input_tensor(interpreter, image)
+# def load_labels(path):  # Read the labels from the text file as a Python list.
+#     with open(path, "r") as f:
+#         return [line.strip() for i, line in enumerate(f.readlines())]
 
-    interpreter.invoke()
-    output_details = interpreter.get_output_details()[0]
-    output = np.squeeze(interpreter.get_tensor(output_details["index"]))
-
-    scale, zero_point = output_details["quantization"]
-    output = scale * (output - zero_point)
-
-    ordered = np.argpartition(-output, 1)
-    return [(i, output[i]) for i in ordered[:top_k]][0]
+# def set_input_tensor(interpreter, image):
+#     tensor_index = interpreter.get_input_details()[0]["index"]
+#     input_tensor = interpreter.tensor(tensor_index)()[0]
+#     input_tensor[:, :] = image
 
 
-model = Interpreter(model_path)
-model.allocate_tensors()
-_, height, width, _ = model.get_input_details()[0]["shape"]
+# def classify_image(interpreter, image, top_k=1):
+#     set_input_tensor(interpreter, image)
 
-for interval in IntervalTimer(10):
-    vid = cv.VideoCapture(0)
-    ret, frame = vid.read()
-    # disable mirror effect
-    frame = cv.flip(frame, 1)
-    # capture image
-    # resize the frame to 224x224
-    frame = cv.resize(frame, (224, 224), interpolation=cv.INTER_AREA)
-    # Make the image a numpy array and reshape it to the models input shape.
-    image = np.asarray(frame, dtype=np.float32).reshape(1, 224, 224, 3)
-    # Normalize the image array
-    image = (image / 127.5) - 1
-    result, prob = classify_image(model, image)
-    labels = load_labels(label_path)
-    classification_label = labels[result]
-    # turn the stepper
-    if result == 0:
-        print("Background")
-        vid.release()
-    elif result == 1:
-        print("Recycle waste")
-        motor.enable(True)
-        motor.run(200 * 8, False)
-        motor.run(200 * 8, True)
-        # enables stepper driver
-        motor.enable(False)
-        vid.release()
-    else:
-        motor.enable(True)
-        motor.run(200 * 8, True)
-        motor.run(200 * 8, False)
-        # enables stepper driver
-        motor.enable(False)
-        vid.release()
+#     interpreter.invoke()
+#     output_details = interpreter.get_output_details()[0]
+#     output = np.squeeze(interpreter.get_tensor(output_details["index"]))
+
+#     scale, zero_point = output_details["quantization"]
+#     output = scale * (output - zero_point)
+
+#     ordered = np.argpartition(-output, 1)
+#     return [(i, output[i]) for i in ordered[:top_k]][0]
+
+
+# model = Interpreter(model_path)
+# model.allocate_tensors()
+# _, height, width, _ = model.get_input_details()[0]["shape"]
+
+# for interval in IntervalTimer(10):
+#     vid = cv.VideoCapture(0)
+#     ret, frame = vid.read()
+#     # disable mirror effect
+#     frame = cv.flip(frame, 1)
+#     # capture image
+#     # resize the frame to 224x224
+#     frame = cv.resize(frame, (224, 224), interpolation=cv.INTER_AREA)
+#     # Make the image a numpy array and reshape it to the models input shape.
+#     image = np.asarray(frame, dtype=np.float32).reshape(1, 224, 224, 3)
+#     # Normalize the image array
+#     image = (image / 127.5) - 1
+#     result, prob = classify_image(model, image)
+#     labels = load_labels(label_path)
+#     classification_label = labels[result]
+#     # turn the stepper
+#     if result == 0:
+#         print("Background")
+#         vid.release()
+#     elif result == 1:
+#         print("Recycle waste")
+#         motor.enable(True)
+#         motor.run(200 * 8, False)
+#         motor.run(200 * 8, True)
+#         # enables stepper driver
+#         motor.enable(False)
+#         vid.release()
+#     else:
+#         motor.enable(True)
+#         motor.run(200 * 8, True)
+#         motor.run(200 * 8, False)
+#         # enables stepper driver
+#         motor.enable(False)
+#         vid.release()
